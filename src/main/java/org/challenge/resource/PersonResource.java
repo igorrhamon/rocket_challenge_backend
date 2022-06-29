ef50b34386f3921e66627637075ad748927d3cb2
Igor <igor-rh@hotmail.com>
2022-06-29 16:06:22 -0300

    Adding @Transactional on Post

--- src/main/java/org/challenge/resource/PersonResource.java
+++ src/main/java/org/challenge/resource/PersonResource.java
@@ -1,11 +1,13 @@
 package org.challenge.resource;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -26,9 +28,10 @@ public class PersonResource {
     }
 
     @POST
+    @Transactional
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createPerson(final @PathParam("name") String name) {
+    public Response createPerson(@QueryParam("name") String name) {
         Person person = new Person();
         person.setName(name);
         personRepository.persist(person);
