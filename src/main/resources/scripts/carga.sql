f7a82b8bf46f82c43e29250d2ba51f2684adb3f4
Igor <igor-rh@hotmail.com>
2022-06-30 14:28:47 -0300

    Carga.sql

--- src/main/resources/scripts/carga.sql
+++ src/main/resources/scripts/carga.sql
@@ -6,5 +6,3 @@ create table Person (id bigint not null, name varchar(255), primary key (id));
 create table Place (id bigint not null, name varchar(255), slug varchar(255), city varchar(255), state varchar(255), created_at timestamp, updated_at timestamp, primary key (id));
 
 create sequence hibernate_sequence start with 1 increment by 1;
-
-INSERT INTO Place (id, name, slug, city, state, created_at, updated_at) VALUES (nextval('hibernate_sequence'), 'São Paulo', 'sao-paulo', 'São Paulo', 'SP', '2018-01-01 00:00:00', '2018-01-01 00:00:00');
--- src/test/resources/scripts/carga_testes.sql
+++ src/test/resources/scripts/carga_testes.sql
@@ -1,4 +1,7 @@
 
 
 
-INSERT INTO Person (id, name) VALUES (nextval('hibernate_sequence'),'John');
+INSERT INTO Person (id, name) VALUES (1,'John');
+
+-- INSERT INTO Place (id, name, slug, city, state) VALUES (nextval('hibernate_sequence'), 'São Paulo', 'sao-paulo', 'São Paulo', 'SP');
+INSERT INTO Place (id, name, slug, city, state) VALUES (1, 'São Paulo', 'sao-paulo', 'São Paulo', 'SP');
